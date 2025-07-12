b6370b2c230f1c6344b3f1da9b42b8363da530bf
keem-hyun <hw813@naver.com>
2025-07-12 21:10:50 +0900

    feat: 일정 관련 domain repository

---
+++ lib/features/trip/domain/repositories/trip_repository.dart
@@ -0,0 +1,9 @@
+import '../entities/trip.dart';
+
+abstract class TripRepository {
+  Future<List<Trip>> getTrips(String userId);
+  Future<Trip> createTrip(Trip trip);
+  Future<Trip> updateTrip(Trip trip);
+  Future<void> deleteTrip(String tripId);
+  Future<Trip?> getTripById(String tripId);
+}
