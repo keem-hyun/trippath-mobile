519c3f68b147d23d76ce1e982eab2c69c205940e
keem-hyun <hw813@naver.com>
2025-07-13 18:41:29 +0900

    add: dotenv 패키지

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:injectable/injectable.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'core/di/injection.dart';
 import 'core/router/app_router.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   configureDependencies(environment: Environment.dev);
   runApp(const ProviderScope(child: MyApp()));
 }
